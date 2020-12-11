7a08cf1bc75c4f4269a566bfdb9dc762979da46d
sbs87 <sbs@stevenbsmith.net>
2020-12-11 17:09:11 -0500

    refactored to loop through any generic triple of node-edge-node given a list. prints out node and edge being used to compute score. also does not mix more than one node or edge at a time

--- DRKG_sandbox_drug_repurposing.py
+++ DRKG_sandbox_drug_repurposing.py
@@ -1,152 +1,125 @@
+"""
+This script will compute TransE scores for a given node-edge-node triplet from DRKG. 
+The nodes and edges are input from lists within seperate files. The nodes and edges are definef in DRKG. 
+
+Author Steven Smith, PhD
+
+"""
+
 import csv
 import pandas as pd
 import numpy as np
 import sys
-
+import csv
 import torch as th
-
-node1=sys.argv[1]
-edge_list=sys.argv[2]
-drug_path=sys.argv[3]
- #'Disease::DOID:2841' #- ashtma
-
-# Rare disease, Mantle cell lymphoma. Associated with translocation in CCND1. Treated with chemotherapy and antibodies 
-# https://www.orpha.net/consor/cgi-bin/Disease_Search.php?lng=EN&data_id=10693&MISSING%20CONTENT=Mantle-cell-lymphoma&search=Disease_Search_Simple&title=Mantle%20cell%20lymphoma
-#3 TOP RESULS DB09107 - Methoxy polyethylene glycol-epoetin beta, stimulates blood cells
-#../DRKG/drkg.tsv:Compound::DB09107	DRUGBANK::target::Compound:Gene	Gene::2057 - Erythropoietin receptor;
-#../DRKG/drkg.tsv:Compound::DB09107	DRUGBANK::treats::Compound:Disease	Disease::MESH:D000740 - anemia
+import torch.nn.functional as fn
 
 
-#./DRKG/drkg.tsv:Compound::DB00188	DRUGBANK::treats::Compound:Disease	Disease::MESH:D020522
-#../DRKG/drkg.tsv:Compound::DB00480	DRUGBANK::treats::Compound:Disease	Disease::MESH:D020522
-#../DRKG/drkg.tsv:Compound::DB09053	DRUGBANK::treats::Compound:Disease	Disease::MESH:D020522
-#../DRKG/drkg.tsv:Compound::DB11703	DRUGBANK::treats::Compound:Disease	Disease::MESH:D020522
-#../DRKG/drkg.tsv:Compound::DB15035	DRUGBANK::treats::Compound:Disease	Disease::MESH:D020522
-disease_list = [
-    node1
-    #"Disease::MESH:D020522"
-    #'Disease::DOID:2841'
-]
+# Read lists, mapping name -> unique ID
+def read_elements(element_path,headers,keyheader):
+    element_list=[]
+    with open(element_path) as csvfile:
+        reader = csv.DictReader(csvfile, delimiter='\t',fieldnames=headers)
+        for row in reader:
+            element_list.append(row[keyheader])
+    return(element_list)
 
-## Expand to all diseases
-drug_list = []
-#infer_path='genes.tsv' #'biotects_withID' #'../DRKG/drugbank_info/drugbank_biotech.txt'
-#infer_path='biotects_withID' #'../DRKG/drugbank_info/drugbank_biotech.txt'
-infer_path=drug_path #'/Users/stevensmith/Projects/DRKG/drug_repurpose/infer_drug.tsv'
-## curate list
-with open(infer_path,newline='',encoding='utf-8') as csvfile:
-     reader=csv.DictReader(csvfile,delimiter='\t',fieldnames=['drug','ids'])
-     for row_val in reader:
-          drug_list.append(row_val['drug'])
+# Computes transE_l2 score: gamma - ||(head + edge - tail)||
+def transE_l2(head, edge, tail, gamma):
+    score = head + edge - tail
+    return gamma - th.norm(score, p=2, dim=-1)
 
+# Map node or edge names to uniuq IDs, handling unknown edge or node names gracefully. 
+def map_components(component_list, mapping_file):
+    ids=[]
+    for component in component_list:
+        try:
+            ids.append(mapping_file[component])
+        except:
+            print("WARNING: skipping unknown "+component)
+    return(ids)
 
-treatment=[edge_list]
-#treatment = ['Hetionet::CtD::Compound:Disease']#,'GNBR::T::Compound:Disease']
-#treatment = ['DRUGBANK::treats::Compound:Disease']
-#treatment = ['GNBR::T::Compound:Disease']
-#treatment = ['GNBR::D::Gene:Disease']
-#treatment = ['DRUGBANK::treats::Compound:Disease',
-#'GNBR::T::Compound:Disease',
-#'Hetionet::CtD::Compound:Disease']
-#treatment = ['GNBR::D::Gene:Disease',
-#'GNBR::G::Gene:Disease',
-#'GNBR::J::Gene:Disease',
-#'GNBR::L::Gene:Disease',
-#'GNBR::Md::Gene:Disease',
-#'GNBR::Te::Gene:Disease',
-#'GNBR::U::Gene:Disease',
-#'GNBR::Ud::Gene:Disease',
-#'GNBR::X::Gene:Disease',
-#'GNBR::Y::Gene:Disease',
-#'Hetionet::DdG::Disease:Gene',
-#'Hetionet::DaG::Disease:Gene',
-#'Hetionet::DuG::Disease:Gene']
-## are these all the combos?
+# Set global variables, structures
 
-entity_idmap_file = '/Users/stevensmith/Projects/DRKG/embed/entities.tsv'
-relation_idmap_file = '/Users/stevensmith/Projects/DRKG/embed/relations.tsv'
+gamma=12.0
+entity_mapping_path = '/Users/stevensmith/Projects/DRKG/embed/entities.tsv'
+edge_mapping_path = '/Users/stevensmith/Projects/DRKG/embed/relations.tsv'
+entity_embeddings = '/Users/stevensmith/Projects/DRKG/embed/DRKG_TransE_l2_entity.npy'
+edge_embeddings = '/Users/stevensmith/Projects/DRKG/embed/DRKG_TransE_l2_relation.npy'
+
 entity_map = {}
 entity_id_map = {}
-relation_map = {}
-with open(entity_idmap_file, newline='', encoding='utf-8') as csvfile:
+edge_map = {}
+edge_id_map = {}
+
+
+# Read node and edge list paths. 
+
+node1_path=sys.argv[1]
+edge_path=sys.argv[2]
+node2_path=sys.argv[3]
+
+
+# Rare node1, Mantle cell lymphoma. Associated with translocation in CCND1. Treated with chemotherapy and antibodies 
+# https://www.orpha.net/consor/cgi-bin/node1_Search.php?lng=EN&data_id=10693&MISSING%20CONTENT=Mantle-cell-lymphoma&search=node1_Search_Simple&title=Mantle%20cell%20lymphoma
+#3 TOP RESULS DB09107 - Methoxy polyethylene glycol-epoetin beta, stimulates blood cells
+
+# Read node and edge lists. 
+
+node1_list=read_elements(node1_path,['node1'],'node1')
+edge_list=read_elements(edge_path,['edge'],'edge')
+node2_list=read_elements(node2_path,['node2','ids'],'node2')
+
+
+# Read in node, edge mapping files
+
+with open(entity_mapping_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t', fieldnames=['name','id'])
     for row_val in reader:
         entity_map[row_val['name']] = int(row_val['id'])
         entity_id_map[int(row_val['id'])] = row_val['name']
 
-with open(relation_idmap_file, newline='', encoding='utf-8') as csvfile:
+with open(edge_mapping_path, newline='', encoding='utf-8') as csvfile:
     reader = csv.DictReader(csvfile, delimiter='\t', fieldnames=['name','id'])
     for row_val in reader:
-        relation_map[row_val['name']] = int(row_val['id'])
-
-drug_ids = []
-disease_ids = []
-for drug in drug_list:
-    drug_ids.append(entity_map[drug])
-for disease in disease_list:
-    disease_ids.append(entity_map[disease])
-
-treatment_rid = [relation_map[treat]  for treat in treatment]
-
-entity_emb = np.load('/Users/stevensmith/Projects/DRKG/embed/DRKG_TransE_l2_entity.npy')
-rel_emb = np.load('/Users/stevensmith/Projects/DRKG/embed/DRKG_TransE_l2_relation.npy')
+        edge_map[row_val['name']] = int(row_val['id'])
+        edge_id_map[int(row_val['id'])] = row_val['name']
 
 
-drug_ids = th.tensor(drug_ids).long()
-disease_ids = th.tensor(disease_ids).long()
-treatment_rid = th.tensor(treatment_rid)
+# Map nodes and edges to their unique ids, convert to tensors
 
-drug_emb = th.tensor(entity_emb[drug_ids])
-treatment_embs = [th.tensor(rel_emb[rid]) for rid in treatment_rid]
-
-
-import torch.nn.functional as fn
-gamma=12.0
-def transE_l2(head, rel, tail):
-    score = head + rel - tail
-    return gamma - th.norm(score, p=2, dim=-1)
-
-def transE_l2_ng(head, rel, tail):
-    score = head + rel - tail
-    return th.norm(score, p=2, dim=-1)
+node1_ids = th.tensor(map_components(node1_list,entity_map)).long()
+node2_ids = th.tensor(map_components(node2_list,entity_map)).long()
+edge_ids = map_components(edge_list,edge_map)
 
 
-scores_per_disease = []
-dids = []
-for rid in range(len(treatment_embs)):
-    treatment_emb=treatment_embs[rid]
-    for disease_id in disease_ids:
-        disease_emb = entity_emb[disease_id]
-        #print("indexes:\nrid:{}\ndiseaseid:{}\n".format(rid,disease_id))
-        #print("shapes:\ndrug: {}\ntreatment:{}\ndisease:{}\n".format(drug_emb.shape, treatment_emb.shape, disease_emb.shape))
-        #print("drug_emb:\n{}\nTreatment_emb:{}\ndisease_emb:{}\n".format(drug_emb, treatment_emb, disease_emb))
-        #print("raw_score={}\nth.norm={}\n".format(drug_emb+treatment_emb-disease_emb,th.norm(drug_emb+treatment_emb-disease_emb,p=2, dim=-1)))
+#node2_ids = th.tensor(node2_ids).long()
+node1_ids = th.tensor(node1_ids).long()
+edge_ids = th.tensor(edge_ids).long()
 
-        score = fn.logsigmoid(transE_l2(drug_emb, treatment_emb, disease_emb))
-        #print("thnorm score:\n{}\ngamma-thnorm\n{}\nlogsignmoid".format(transE_l2_ng(drug_emb, treatment_emb, disease_emb),transE_l2(drug_emb, treatment_emb, disease_emb),score))
-        scores_per_disease.append(score)
-        dids.append(drug_ids)
-scores = th.cat(scores_per_disease)
-dids = th.cat(dids)
-idx = th.flip(th.argsort(scores), dims=[0])
-scores = scores[idx].numpy()
-dids = dids[idx].numpy()
+# Read in entity and edge embeddings
+entity_emb = np.load( entity_embeddings)
+edge_emb = np.load(edge_embeddings)
 
-_, unique_indices = np.unique(dids, return_index=True)
-
-topk=100
-topk_indices = np.sort(unique_indices)[:topk]
-proposed_dids = dids[topk_indices]
-proposed_scores = scores[topk_indices]
-for i in range(topk):
-    drug = int(proposed_dids[i])
-    score = proposed_scores[i]
-    
-    print("{}\t{}".format(entity_id_map[drug], score))
+# Filter for node1, node2 and edge embeddings of interest
+node1_embs = {rid:th.tensor(entity_emb[rid]) for rid in node1_ids}
+node2_embs = {rid:th.tensor(entity_emb[rid]) for rid in node2_ids}
+edge_embs = {rid:th.tensor(edge_emb[rid]) for rid in edge_ids}
 
 
-#1. Generate scores for x edges for y disease 
-#2. widen dataset
 
-#tSIne of embeddings for 
+""""
+Compute TransE
+"""
 
+# Compute node1-edge-node2 transE score and print. 
+for (node1_i, node1_i_emb) in node1_embs.items():
+    for (edge_i, edge_i_emb) in edge_embs.items():
+        for(node2_i, node2_i_emb) in node2_embs.items():
+            score = fn.logsigmoid(transE_l2(node1_i_emb, edge_i_emb, node2_i_emb, gamma))
+            node1_name=entity_id_map[int(node1_i)]
+            edge_name=edge_id_map[int(edge_i)]
+            node2_name=entity_id_map[int(node2_i)]
+            print("{}\t{}\t{}\t{}".format(node1_name,edge_name,node2_name,score))
+
+## END
